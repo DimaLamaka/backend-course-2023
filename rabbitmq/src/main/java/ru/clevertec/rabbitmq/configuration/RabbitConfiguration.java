5a1e8735d6d65fd64966f2d78763fe451de02e73
Ламако Дмитрий Юрьевич <DLamako@alfabank.ru>
2023-12-15 09:09:36 +0300

    added rabbitmq

---
+++ rabbitmq/src/main/java/ru/clevertec/rabbitmq/configuration/RabbitConfiguration.java
@@ -0,0 +1,10 @@
+package ru.clevertec.rabbitmq.configuration;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitConfiguration {
+
+}
